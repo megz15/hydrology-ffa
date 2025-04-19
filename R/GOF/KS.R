8184e36e1a13c134b41f4eba959b13e78f60ddd0
megz15 <meghraj.g16@gmail.com>
2025-04-19 22:34:43 +0530

    chore

--- R/GOF/KS.R
+++ R/GOF/KS.R
@@ -1,19 +1,19 @@
 # Kolmogorov-Smirnov
 
-ks_gumbel_mle <- ks.test(ams_data$ams, "pgev",
+ks_gumbel_mle = ks.test(ams_data$ams, "pgev",
                          loc = gumbel_fit$results$par["location"],
                          scale = gumbel_fit$results$par["scale"],
                          shape = 0)
 
-ks_lognormal_mle <- ks.test(ams_data$ams, "plnorm",
+ks_lognormal_mle = ks.test(ams_data$ams, "plnorm",
                             meanlog = lognormal_fit$estimate["meanlog"],
                             sdlog = lognormal_fit$estimate["sdlog"])
 
-ks_gamma_mle <- ks.test(ams_data$ams, "pgamma",
+ks_gamma_mle = ks.test(ams_data$ams, "pgamma",
                         shape = gamma_fit$estimate["shape"],
                         rate = gamma_fit$estimate["rate"])
 
-ks_weibull_mle <- ks.test(ams_data$ams, "pweibull",
+ks_weibull_mle = ks.test(ams_data$ams, "pweibull",
                           shape = weibull_fit$estimate["shape"],
                           scale = weibull_fit$estimate["scale"])
 
